917067ec87dcc1100ca40a5d05778ba7a2cab948
KirkADuran <kirkaduran@gmail.com>
2025-04-09 14:28:57 -0700

    Create test_student.py

---
+++ test_student.py
@@ -0,0 +1,15 @@
+import unittest
+from proj1 import *
+#proj1.py should contain your data class and function definitions
+
+class TestRegionFunctions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_holder(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
